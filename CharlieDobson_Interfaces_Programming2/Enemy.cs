511faabaa9b28dedeb82dcc3a9e947631e5c6aae
C-DGameMaker <charlotte5634@outlook.com>
2026-01-29 09:20:15 -0400

    Can Change Enemy State

--- CharlieDobson_Interfaces_Programming2/Enemy.cs
+++ CharlieDobson_Interfaces_Programming2/Enemy.cs
@@ -9,7 +9,7 @@ namespace CharlieDobson_Interfaces_Programming2
     internal class Enemy
     {
         public ConsoleColor _enemyColor;
-        Position _enemyPosition;
+        public Position _enemyPosition { get; private set; }
 
         public Enemy(Position enemyPosition, ConsoleColor enemyColor)
         {
@@ -24,27 +24,40 @@ namespace CharlieDobson_Interfaces_Programming2
             random
         };
 
-        MovementStragety _movementStraety;
+        MovementStragety _movementStragety;
+        Random _random = new Random();
+        public char _enemyState;
 
         public void Move()
         {
-            if(_movementStraety == MovementStragety.aggresive)
+            if(_movementStragety == MovementStragety.aggresive)
             {
 
             }
-            else if(_movementStraety == MovementStragety.passive)
+            else if(_movementStragety == MovementStragety.passive)
             {
 
             }
-            else if(_movementStraety == MovementStragety.random)
+            else if(_movementStragety == MovementStragety.random)
             {
-
+                int direction = _random.Next(1,5);
             }
         }
 
         public void ChangeMovementStragety()
         {
-            
+            if(_enemyState == 'i')
+            {
+                _movementStragety = MovementStragety.aggresive;
+            }
+            else if (_enemyState == 'o')
+            {
+                _movementStragety = MovementStragety.passive;
+            }
+            else if (_enemyState == 'p')
+            {
+                _movementStragety = MovementStragety.random;
+            }
         }
 
     }
--- CharlieDobson_Interfaces_Programming2/Program.cs
+++ CharlieDobson_Interfaces_Programming2/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,14 +9,38 @@ namespace CharlieDobson_Interfaces_Programming2
 {
     internal class Program
     {
+        
         static void Main(string[] args)
         {
-            Position _playerPosition = new Position(10, 10);
-            Player _player = new Player(playerPosition: _playerPosition, color: ConsoleColor.Blue);
+             Player _player = new Player(playerPosition: new Position(10, 10), color: ConsoleColor.Blue);
+             Enemy _enemy = new Enemy(enemyPosition: new Position(0, 10), enemyColor: ConsoleColor.DarkRed);
 
-            Console.SetCursorPosition(_player._playerPosition._xPos, _player._playerPosition._xPos);
-            Console.ForegroundColor = _player._playerColor;
-            Console.Write("*");
+            char _enemyChar;
+
+            while(true)
+            {
+                _enemyChar = Console.ReadKey().KeyChar;
+
+                if(_enemyChar == 'm')
+                {
+                    _enemy.Move();
+                    
+                }
+                if(_enemyChar == 'i' || _enemyChar == 'o' || _enemyChar == 'p')
+                {
+                    _enemy._enemyState = _enemyChar;
+                    _enemy.ChangeMovementStragety();
+                }
+
+                Console.SetCursorPosition(_player._playerPosition._xPos, _player._playerPosition._yPos);
+                Console.ForegroundColor = _player._playerColor;
+                Console.Write("*");
+
+                Console.SetCursorPosition(_enemy._enemyPosition._xPos, _enemy._enemyPosition._yPos);
+                Console.ForegroundColor = _enemy._enemyColor;
+                Console.Write("*");
+            }
         }
+
     }
 }
