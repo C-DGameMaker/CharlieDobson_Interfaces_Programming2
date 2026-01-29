ba0527ce9b6551072b11ac8252b538c6dc08e96d
C-DGameMaker <charlotte5634@outlook.com>
2026-01-29 15:04:12 -0400

    Added new classes

---
+++ CharlieDobson_Interfaces_Programming2/IMoveStragety.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharlieDobson_Interfaces_Programming2
+{
+    internal interface IMoveStragety
+    {
+        Position Move(Position position);
+    }
+}
---
+++ CharlieDobson_Interfaces_Programming2/RandomMovementStragety.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharlieDobson_Interfaces_Programming2
+{
+    internal class RandomMovementStragety : IMoveStragety
+    {
+        public Position Move(Position position)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
